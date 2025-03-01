eb5b34b5af891db1280b4d70e885966aa5aca000
Amiramohamed <amiramohmaed887@gmail.com>
2025-03-01 16:29:10 +0200

    Refactor main.dart to implement a bottom navigation bar and multiple screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+  DateTime? lastPressed;
+
+  final List<Widget> _screens = [
+    Center(child: Text("Home Page🏠")),
+    Center(child: Text("Rides 🚗")),
+    Center(child: Text("Communities 👥")),
+    Center(child: Text("Messages 💬")),
+    Center(child: Text("Notifications 🔔")),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(title: Text("Pop Scope Demo")),
+      body: _screens[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.directions_car), label: "rides"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.groups), label: "communities"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "messages"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: "notifications"),
+        ],
       ),
     );
   }
